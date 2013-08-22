c5a479fafd63abfab3047041cc1626456c131e94
Tomás Lucas Vilaboa <tvilaboa@hotmail.com>
2013-08-22 16:22:15 -0300

    Add ObservableSorter,SelectionSorter,SorterListener interfaces

---
+++ src/main/java/anaydis/sort/SorterListener.java
@@ -0,0 +1,11 @@
+package anaydis.sort;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Tomás Vilaboa
+ * Project: anaydis-2013
+ * Date: 22/08/13
+ * Time: 16:18
+ */
+public interface SorterListener {
+}
