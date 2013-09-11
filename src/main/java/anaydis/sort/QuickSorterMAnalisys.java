941324634447ba356660f2e9dd66c2f3c3672f46
Tomás Lucas Vilaboa <tvilaboa@hotmail.com>
2013-09-11 18:12:58 -0300

    Modify QuickSorterMAnalisys to copy the same random list for every M so the comparison depends only on the M and not on the list order.

--- src/main/java/anaydis/sort/QuickSorterMAnalisys.java
+++ src/main/java/anaydis/sort/QuickSorterMAnalisys.java
@@ -14,18 +14,19 @@ public class QuickSorterMAnalisys {
     public static void main(String[] args) {
         for(int i = 1000;i<=1000000;i*=10){
              test(i);
-            break;
         }
     }
 
+    //Copy the same list for every M so the the time depends only on the M and not of the list orden.
     private static void test(int N) {
         QuickSorterCutOff quick=new QuickSorterCutOff();
         IntDataSetGenerator generator=new IntDataSetGenerator();
         List<Integer> list;
         long recordTime=999999999;
         int m=0;
+        List<Integer> originalList=generator.createRandom(N);
         for(int i=5;i<=25;i++){
-            list=generator.createDescending(N);
+            list=originalList.subList(0,N-1);
             long time= System.currentTimeMillis();
             quick.sort(new MyIntComparable(),list,i);
             time= (System.currentTimeMillis()-time);
